29a82058964eb9d10beafdc8ed8f8a145a493704
Mateusz <mateusz.sioja@gmail.com>
2017-04-01 12:47:22 +0200

    init

---
+++ src/main/java/com/msjs/managementservice/ManagementServiceServerApplication.java
@@ -0,0 +1,12 @@
+package com.msjs.managementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementServiceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementServiceServerApplication.class, args);
+	}
+}
